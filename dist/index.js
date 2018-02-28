12699838a3eaf1f58e2c5cae424a6927e27e52af
zhouhuafei <zhouhuafei@shopex.cn>
2018-02-28 17:19:26 +0800

    module

--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,49 @@
-"use strict";
+'use strict';
 
-function sku(arr) {
-    var length = arr.length;
-    var num = 0;
-    var result = [];
-    if (length !== 0) {
-        arr[0].forEach(function (v) {
-            result.push([v]);
-        });
-        fn(arr);
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+(function (name, factory) {
+    if ((typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined') {
+        // nodejs - commonjs canon
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // requirejs - amd canon
+        define(factory);
+    } else {
+        // window - browser canon
+        if (Object.prototype.toString.call(window.zhf).slice(8, -1).toLowerCase() !== 'object') {
+            window.zhf = {};
+        }
+        window.zhf[name] = factory();
     }
-
-    function fn(arr) {
-        num++;
-        if (num < length) {
-            var r = [];
-            result.forEach(function (v1) {
-                arr[num].forEach(function (v2) {
-                    r.push(v1.concat(v2));
-                });
+})('sku', function () {
+    function sku(arr) {
+        var length = arr.length;
+        var num = 0;
+        var result = [];
+        if (length !== 0) {
+            arr[0].forEach(function (v) {
+                result.push([v]);
             });
-            result = r;
             fn(arr);
         }
+
+        function fn(arr) {
+            num++;
+            if (num < length) {
+                var r = [];
+                result.forEach(function (v1) {
+                    arr[num].forEach(function (v2) {
+                        r.push(v1.concat(v2));
+                    });
+                });
+                result = r;
+                fn(arr);
+            }
+        }
+
+        return result;
     }
 
-    return result;
-}
-
-module.exports = sku;
+    return sku;
+});
--- src/index.js
+++ src/index.js
@@ -1,29 +1,42 @@
-function sku(arr) {
-    const length = arr.length;
-    let num = 0;
-    let result = [];
-    if (length !== 0) {
-        arr[0].forEach(function (v) {
-            result.push([v]);
-        });
-        fn(arr);
+(function (name, factory) {
+    if (typeof exports === 'object' && typeof module !== 'undefined') { // nodejs - commonjs canon
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) { // requirejs - amd canon
+        define(factory);
+    } else { // window - browser canon
+        if (Object.prototype.toString.call(window.zhf).slice(8, -1).toLowerCase() !== 'object') {
+            window.zhf = {};
+        }
+        window.zhf[name] = factory();
     }
-
-    function fn(arr) {
-        num++;
-        if (num < length) {
-            const r = [];
-            result.forEach(function (v1) {
-                arr[num].forEach(function (v2) {
-                    r.push(v1.concat(v2));
-                });
+})('sku', function () {
+    function sku(arr) {
+        const length = arr.length;
+        let num = 0;
+        let result = [];
+        if (length !== 0) {
+            arr[0].forEach(function (v) {
+                result.push([v]);
             });
-            result = r;
             fn(arr);
         }
+
+        function fn(arr) {
+            num++;
+            if (num < length) {
+                const r = [];
+                result.forEach(function (v1) {
+                    arr[num].forEach(function (v2) {
+                        r.push(v1.concat(v2));
+                    });
+                });
+                result = r;
+                fn(arr);
+            }
+        }
+
+        return result;
     }
 
-    return result;
-}
-
-module.exports = sku;
+    return sku;
+});
